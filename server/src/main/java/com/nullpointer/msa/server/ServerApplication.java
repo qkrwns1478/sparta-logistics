1bd3ec916e64bdedf523c1fc44c9d7f5878bbabd
Dahye Lee <chaelee1111@naver.com>
2026-05-06 12:56:57 +0900

    init commit

---
+++ server/src/main/java/com/nullpointer/msa/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.nullpointer.msa.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
