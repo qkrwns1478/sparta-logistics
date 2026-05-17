28fb7f6da1410aa72d89c90051a02d78d405ee92
euneuneun <daeun2597@gmail.com>
2026-05-17 21:55:02 +0900

    feat: BaseEntity, 공통 예외 처리, 응답 규격

---
+++ common/src/main/java/com/example/msa/common/response/ApiResponse.java
@@ -0,0 +1,39 @@
+package com.example.msa.common.response;
+
+import com.example.msa.common.exception.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T>(
+        boolean success,
+        int status,
+        String erroCode,
+        String message,
+        T data
+) {
+
+    // 성공
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true ,200,null, "OK",data);
+    }
+
+    // 커스텀 메세지 포함 성공
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, 200, null, message, data);
+    }
+
+    // 성공(바디없음)
+    public static <T> ApiResponse<T> noContent() {
+        return new ApiResponse<>(true,200, null,"OK",null);
+    }
+
+    // 에러
+    public static <T> ApiResponse<T> error(ErrorCode errorCode) {
+        return new ApiResponse<>(false,  errorCode.getStatus().value(), errorCode.getCode(),errorCode.getMessage(),null);
+    }
+
+    // 커스텀 메세지 포함 에러
+    public static <T> ApiResponse<T> error(ErrorCode errorCode, String message) {
+        return new ApiResponse<>(false,  errorCode.getStatus().value(), errorCode.getCode(), message, null);
+    }
+}
