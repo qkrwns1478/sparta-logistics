3dc94cd7aea4c5e91caa1c242b24d6b99495d684
euneuneun <daeun2597@gmail.com>
2026-05-19 04:26:58 +0900

    refactor: BaseEntity 수정 및 공통 응답 규격 변경

---
+++ common/src/main/java/com/sparta/logistics/common/config/AuditorAwareConfig.java
@@ -0,0 +1,27 @@
+package com.sparta.logistics.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+public class AuditorAwareConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication == null || !authentication.isAuthenticated()
+                    || authentication.getName().equals("anonymousUser")) {
+                return Optional.of("system"); // 비인증 상태 fallback
+            }
+
+            return Optional.of(authentication.getName()); // username or userId
+        };
+    }
+}
--- common/src/main/java/com/sparta/logistics/common/response/ApiResponse.java
+++ common/src/main/java/com/sparta/logistics/common/response/ApiResponse.java
@@ -7,33 +7,32 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 public record ApiResponse<T>(
         boolean success,
         int status,
-        String errorCode,
         String message,
         T data
 ) {
 
     // 성공
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true ,200,null, "OK",data);
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(true ,200, "OK",data);
     }
 
     // 커스텀 메세지 포함 성공
-    public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>(true, 200, null, message, data);
+    public static <T> ApiResponse<T> ok(String message, T data) {
+        return new ApiResponse<>(true, 200, message, data);
     }
 
     // 성공(바디없음)
     public static <T> ApiResponse<T> noContent() {
-        return new ApiResponse<>(true,200, null,"OK",null);
+        return new ApiResponse<>(true,200, "OK",null);
     }
 
     // 에러
     public static <T> ApiResponse<T> error(ErrorCode errorCode) {
-        return new ApiResponse<>(false,  errorCode.getStatus().value(), errorCode.getCode(),errorCode.getMessage(),null);
+        return new ApiResponse<>(false, 400,errorCode.getMessage(),null);
     }
 
     // 커스텀 메세지 포함 에러
     public static <T> ApiResponse<T> error(ErrorCode errorCode, String message) {
-        return new ApiResponse<>(false,  errorCode.getStatus().value(), errorCode.getCode(), message, null);
+        return new ApiResponse<>(false,  400, message, null);
     }
 }
