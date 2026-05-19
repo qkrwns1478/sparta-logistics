9c1cc51d300904a7c141258ad92771757cb7f944
Dahye Lee <chaelee1111@naver.com>
2026-05-19 19:48:56 +0900

    config: Enable JPA Auditing 설정 추가 (#4)

--- company-service/src/main/java/com/sparta/logistics/company/CompanyServiceApplication.java
+++ company-service/src/main/java/com/sparta/logistics/company/CompanyServiceApplication.java
@@ -3,9 +3,11 @@ package com.sparta.logistics.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
 public class CompanyServiceApplication {
 
     public static void main(String[] args) {
