8781de86d91a1cfe7224a1a5b697a05504a222ef
look516 <happycatcindy@gmail.com>
2026-05-19 12:22:48 +0900

    feat: delivery service init setting

---
+++ delivery-service/src/test/java/com/example/delivery/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullpointer.msa.delivery-service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
