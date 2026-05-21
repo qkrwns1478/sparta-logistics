b47ed496ef5eff037cd61126ba04d7a04e83d139
Dahye Lee <chaelee1111@naver.com>
2026-05-22 02:57:29 +0900

    Revert "fix: Company Service 실행 오류 수정 (JPQL 문법 오류 및 JPA Auditing 중복 설정) (#85)"
    This reverts commit 690777fcbeac7a96d9f87718a0c530ffe573cc19.

--- company-service/src/main/java/com/sparta/logistics/company/CompanyServiceApplication.java
+++ company-service/src/main/java/com/sparta/logistics/company/CompanyServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
         "com.sparta.logistics.common"
 })
 @EnableFeignClients
+@EnableJpaAuditing
 public class CompanyServiceApplication {
 
     public static void main(String[] args) {
