a4589a0fd79899a82fa218ee51c79a3db5d62df0
look516 <happycatcindy@gmail.com>
2026-05-25 20:37:30 +0900

    chore: 배송 서비스 불필요한 주석 제거(#135)

--- delivery-service/src/main/java/DeliveryServiceApplication.java
+++ delivery-service/src/main/java/DeliveryServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 		"com.sparta.logistics.common"
 })
 @EnableFeignClients
-// TODO: @EnableJpaAuditing 추가였는지 아니었는지 기억
 public class DeliveryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryServiceApplication.class, args);
