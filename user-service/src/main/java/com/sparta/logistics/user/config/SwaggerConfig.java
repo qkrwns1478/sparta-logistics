6740600864ab4d67090fffa21a4699939cc076c5
euneuneun <daeun2597@gmail.com>
2026-05-29 07:22:26 +0900

    fix: SwaggerConfig 수정

--- user-service/src/main/java/com/sparta/logistics/user/config/SwaggerConfig.java
+++ user-service/src/main/java/com/sparta/logistics/user/config/SwaggerConfig.java
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String JWT_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public GroupedOpenApi userApi() {
         return GroupedOpenApi.builder()
@@ -23,18 +25,15 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        String jwtSchemeName = "bearerAuth";
-
         return new OpenAPI()
                 .addServersItem(new Server().url("/"))
                 .info(new Info()
                         .title("User Service API")
                         .description("유저 서비스 API 명세서")
                         .version("v1"))
-                .addSecurityItem(new SecurityRequirement().addList(jwtSchemeName))
+                .addSecurityItem(new SecurityRequirement().addList(JWT_SCHEME_NAME))
                 .components(new Components()
-                        .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
-                                .name(jwtSchemeName)
+                        .addSecuritySchemes(JWT_SCHEME_NAME, new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
