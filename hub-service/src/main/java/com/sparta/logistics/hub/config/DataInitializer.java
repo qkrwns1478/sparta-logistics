c4d7b4046e09d033585d0fb689cc820445b9abc7
김승현 <shkim7020@gmail.com>
2026-05-29 10:44:22 +0900

    fix: DataInitializer에 @Profile("!test") 추가 (#240)

--- hub-service/src/main/java/com/sparta/logistics/hub/config/DataInitializer.java
+++ hub-service/src/main/java/com/sparta/logistics/hub/config/DataInitializer.java
@@ -6,11 +6,13 @@ import com.sparta.logistics.hub.hub.service.HubService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Profile("!test")
 @Component
 @RequiredArgsConstructor
 @Order(1)
